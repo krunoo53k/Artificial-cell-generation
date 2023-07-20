06d4519dff5ef8aa5f42a023261c29cdb6b3c036
krunoo53k <petrik.kpetrik@gmail.com>
2023-07-20 12:58:20 +0200

    adding noise function

--- src/scripts/gen_blob.py
+++ src/scripts/gen_blob.py
@@ -32,6 +32,45 @@ def interpolateSmoothly(xs, N):
     fs2 = fs[:half] + [0]*(len(fs)*N) + fs[half:]
     return [x.real / len(xs) for x in dft(fs2)[::-1]]
 
+def noisy(noise_typ,image):
+    if noise_typ == "gauss":
+      row,col,ch= image.shape
+      mean = 0
+      var = 0.1
+      sigma = var**0.5
+      gauss = np.random.normal(mean,sigma,(row,col,ch))
+      gauss = gauss.reshape(row,col,ch)
+      noisy = image + gauss
+      return noisy
+    elif noise_typ == "s&p":
+      row,col,ch = image.shape
+      s_vs_p = 0.5
+      amount = 0.004
+      out = np.copy(image)
+      # Salt mode
+      num_salt = np.ceil(amount * image.size * s_vs_p)
+      coords = [np.random.randint(0, i - 1, int(num_salt))
+              for i in image.shape]
+      out[coords] = 1
+
+      # Pepper mode
+      num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+      coords = [np.random.randint(0, i - 1, int(num_pepper))
+              for i in image.shape]
+      out[coords] = 0
+      return out
+    elif noise_typ == "poisson":
+        vals = len(np.unique(image))
+        vals = 2 ** np.ceil(np.log2(vals))
+        noisy = np.random.poisson(image * vals) / float(vals)
+        return noisy
+    elif noise_typ =="speckle":
+        row,col,ch = image.shape
+        gauss = np.random.randn(row,col,ch)
+        gauss = gauss.reshape(row,col,ch)        
+        noisy = image + image * gauss
+        return noisy
+
 pts = [(random() + 0.8) * cmath.exp(2j*cmath.pi*i/7) for i in range(7)]
 pts = convexHull([(pt.real, pt.imag ) for pt in pts])
 xs, ys = [interpolateSmoothly(zs, 30) for zs in zip(*pts)]
@@ -64,4 +103,9 @@ points = points.reshape((-1, 1, 2))
 cv.fillPoly(img, [points], color=255)
 
 plt.imshow(img)
+plt.show()
+
+img2 = noisy("s&p", img)
+
+plt.imshow(img2)
 plt.show()
