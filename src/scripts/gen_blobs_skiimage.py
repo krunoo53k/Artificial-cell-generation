26542076aca3475520d6da8a874f51b03475ede1
krunoo53k <petrik.kpetrik@gmail.com>
2024-01-19 16:19:09 +0100

    Expermenting with different methods

--- src/scripts/gen_blobs_skiimage.py
+++ src/scripts/gen_blobs_skiimage.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.draw import random_shapes
+import matplotlib.pyplot as plt
 
 def generate_unconventional_blobs(num_blobs=3, shape='circle', size=100, max_size=100):
     blobs = []
@@ -8,7 +9,8 @@ def generate_unconventional_blobs(num_blobs=3, shape='circle', size=100, max_siz
         blobs.append(image.astype(float))
     return blobs
 
-blobs = generate_unconventional_blobs(num_blobs=3, shape='star', size=30, max_size=60)
+"""
+blobs = generate_unconventional_blobs(num_blobs=3, shape='circle', size=30, max_size=60)
 
 image = np.zeros((360, 363))
 for blob in blobs:
@@ -17,4 +19,8 @@ for blob in blobs:
     x_end = x_start + blob.shape[0]
     y_end = y_start + blob.shape[1]
     image[x_start:x_end, y_start:y_end] = blob
+"""
 
+image, points = random_shapes((128, 128), shape='circle', max_size=128, min_size=72, min_shapes=1, max_shapes=1)
+plt.imshow(image)
+plt.show()
