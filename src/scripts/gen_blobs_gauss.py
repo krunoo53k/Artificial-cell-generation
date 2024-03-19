0146b90e95caa6e548ffaca7e533010db66151ae
kruno <petrik.kpetrik@gmail.com>
2024-03-19 15:04:49 +0100

    Adding colored gaussian blobs with alpha

--- src/scripts/gen_blobs_gauss.py
+++ src/scripts/gen_blobs_gauss.py
@@ -1,32 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.ndimage.filters import gaussian_filter
+import random
+from math import pi
+from utils import transform_blob
 
 blob_size = 128
 
-def generate_blobs(num_blobs=9, blob_size=blob_size, x_std=10, y_std=20):
-    blobs = []
-    for i in range(num_blobs):
-        x_center = np.random.randint(0, blob_size)
-        y_center = np.random.randint(0, blob_size)
-        x, y = np.meshgrid(np.arange(blob_size), np.arange(blob_size))
-        blob = np.exp(-((x - x_center)**2 / (2 * x_std**2) + (y - y_center)**2 / (2 * y_std**2)))
-        blobs.append(blob)
-    return blobs
 
-blobs = generate_blobs()
+def gaus2d(x=0, y=0, mx=0, my=0, sx=1, sy=1, theta=0):
+    x_rot = (x - mx) * np.cos(theta) - (y - my) * np.sin(theta)
+    y_rot = (x - mx) * np.sin(theta) + (y - my) * np.cos(theta)
+    return 1. / (2. * np.pi * sx * sy) * np.exp(-((x_rot ** 2. / (2. * sx ** 2.)) + (y_rot ** 2. / (2. * sy ** 2.))))
 
-image = np.zeros((360,363))
 
-for i, blob in enumerate(blobs):
-    x_start = np.random.randint(0, 360 - blob_size)
-    y_start = np.random.randint(0, 363 - blob_size)
-    image[x_start:x_start + blob_size, y_start:y_start + blob_size] = blob
-    #plt.imshow(blob, cmap='gray')
-    #plt.title(f'Blob {i}')
-    #plt.show()
+def generate_blob(size=128):
+    x = np.linspace(-5, 5, size)
+    y = np.linspace(-5, 5, size)
+    x, y = np.meshgrid(x, y)  # Get 2D variables instead of 1D
 
-blurred = gaussian_filter(image, sigma = 7)
-plt.imshow(blurred, cmap='gray')
-plt.title('Final image')
-plt.show()
+    sx = random.uniform(1, 2.5)
+    sy = random.uniform(1, 2.5)
+    theta = random.uniform(-2 * pi, 2 * pi)
+
+    z = gaus2d(x, y, mx=0, my=0, sx=sx, sy=sy, theta=theta)
+    z = (z - np.min(z)) / (np.max(z) - np.min(z))
+    z[z <= 0.2] = 0
+    z[z != 0] = 1
+    return z
+
+
+def color_blob(blob):
+    image = transform_blob(blob)
+    cell_color = np.array((0.64, 0.54, 0.6, 0))
+    colored_image = np.full((128, 128, 4), cell_color)
+    colored_image[:, :, 3] = image
+    return colored_image
+
+
+if __name__ == "__main__":
+    blob_img = generate_blob()
+    img = color_blob(blob_img)
+    plt.imshow(img)
+    plt.show()
