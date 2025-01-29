e4b99a406e4bd6339abc4ca67a89b00b39eacbbf
kruno <petrik.kpetrik@gmail.com>
2025-01-29 16:06:15 +0100

    test: updating tests for new parameters

--- tests/test_nucelus.py
+++ tests/test_nucelus.py
@@ -12,21 +12,30 @@ def test_neutrophil_nucleus_generation():
             num_segments=3,
             segment_frequency=2.0,
             segment_amplitude=0.3,
-            base_thickness=0.3
+            base_thickness=0.4,
+            connection_thickness=0.15,
+            compactness=0.3,
+            curve_randomness=0.2
         ),
         NeutrophilNucleusParams(
             size=512,
             num_segments=2,
             segment_frequency=1.5,
             segment_amplitude=0.4,
-            base_thickness=0.35
+            base_thickness=0.45,
+            connection_thickness=0.12,
+            compactness=0.25,
+            curve_randomness=0.15
         ),
         NeutrophilNucleusParams(
             size=512,
             num_segments=4,
             segment_frequency=3.0,
             segment_amplitude=0.25,
-            base_thickness=0.25
+            base_thickness=0.35,
+            connection_thickness=0.1,
+            compactness=0.4,
+            curve_randomness=0.25
         )
     ]
 
@@ -38,7 +47,7 @@ def test_neutrophil_nucleus_generation():
 
         ax.imshow(image, cmap='Blues')
         ax.set_title(f'Segments: {params.num_segments}\n'
-                    f'Freq: {params.segment_frequency}')
+                    f'Compact: {params.compactness}')
         ax.axis('off')
 
     plt.tight_layout()
