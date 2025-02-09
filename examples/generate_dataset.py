d30e2c90b23fd2771ea263bf820657206580483b
kruno <petrik.kpetrik@gmail.com>
2025-02-09 15:24:09 +0100

    feat: add monocyte generation to examples

--- examples/generate_dataset.py
+++ examples/generate_dataset.py
@@ -47,5 +47,5 @@ if __name__ == "__main__":
     generate_dataset(
         output_dir="output/dataset_v1",
         num_images=10,
-        cell_types=["neutrophil"]
+        cell_types=["neutrophil", "monocyte"]
     )
